a2cdfaa68cd87a46bc20021565556fb76abd5c4f
@me.Jaysandoval <sandovaljayemmanuel@gmail.com>
2026-06-12 19:07:22 +0800

    changing mail provider

--- Lucky14/Lucky14/Lucky14/src/main/java/PawerOpp/Lucky14/config/MailConfig.java
+++ Lucky14/Lucky14/Lucky14/src/main/java/PawerOpp/Lucky14/config/MailConfig.java
@@ -20,6 +20,8 @@ public class MailConfig {
             @Value("${spring.mail.properties.mail.smtp.auth:true}") boolean auth,
             @Value("${spring.mail.properties.mail.smtp.starttls.enable:true}") boolean starttls,
             @Value("${spring.mail.properties.mail.smtp.starttls.required:true}") boolean starttlsRequired,
+            @Value("${spring.mail.properties.mail.smtp.ssl.enable:false}") boolean sslEnabled,
+            @Value("${spring.mail.properties.mail.smtp.ssl.trust:}") String sslTrust,
             @Value("${spring.mail.properties.mail.smtp.connectiontimeout:5000}") int connectionTimeout,
             @Value("${spring.mail.properties.mail.smtp.timeout:5000}") int timeout,
             @Value("${spring.mail.properties.mail.smtp.writetimeout:5000}") int writeTimeout
@@ -34,6 +36,10 @@ public class MailConfig {
         props.put("mail.smtp.auth", String.valueOf(auth));
         props.put("mail.smtp.starttls.enable", String.valueOf(starttls));
         props.put("mail.smtp.starttls.required", String.valueOf(starttlsRequired));
+        props.put("mail.smtp.ssl.enable", String.valueOf(sslEnabled));
+        if (sslTrust != null && !sslTrust.isBlank()) {
+            props.put("mail.smtp.ssl.trust", sslTrust.trim());
+        }
         props.put("mail.smtp.connectiontimeout", String.valueOf(connectionTimeout));
         props.put("mail.smtp.timeout", String.valueOf(timeout));
         props.put("mail.smtp.writetimeout", String.valueOf(writeTimeout));
